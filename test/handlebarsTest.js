67f23f59f274c583d02b768147ab6424a19b3e48
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-13 14:47:23 +0200

    add template caching into release 1

--- test/handlebarsTest.js
+++ test/handlebarsTest.js
@@ -4,33 +4,33 @@ var handlebars = require("../lib/handlebarsEngine.js");
 describe('handlebars', function(){
 
   it('should render html', function () {
-    var html = handlebars("Hey", null, null);
+    var html = handlebars("Hey")(null, null);
     html.should.be.eql("Hey");
   });
 
   it('should be able to use helpers', function () {
-    var html = handlebars("{{{a}}}", { a: function() { return "Hey"; } }, null);
+    var html = handlebars("{{{a}}}")({ a: function() { return "Hey"; } }, null);
     html.should.be.eql("Hey");
   });
 
   it('should be able to use data', function () {
-    var html = handlebars("{{{a}}}", null, { a: "Hey" });
+    var html = handlebars("{{{a}}}")(null, { a: "Hey" });
     html.should.be.eql("Hey");
   });
 
   it('should throw when syntax error', function () {
     should(function () {
-      handlebars("{{#if}}", null, {});
+      handlebars("{{#if}}")(null, {});
     }).throw();
   });
 
   it('should work with jsreport syntax', function () {
-    var html = handlebars("{#image {{b}}}", null, { b : "foo"});
+    var html = handlebars("{#image {{b}}}")(null, { b : "foo"});
     html.should.be.eql("{#image foo}");
   });
 
   it('should work with jsreport syntax in many places', function() {
-    var html = handlebars("<img src='{#image {{name}}}'/><img src='{#image {{name2}}}'/>", null, { name : "foo", name2: "bar"});
+    var html = handlebars("<img src='{#image {{name}}}'/><img src='{#image {{name2}}}'/>")(null, { name : "foo", name2: "bar"});
     html.should.be.eql("<img src='{#image foo}'/><img src='{#image bar}'/>");
   });
 });
