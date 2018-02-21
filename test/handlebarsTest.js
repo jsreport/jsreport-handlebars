27a7e7d74242186f20acb817743cfaef639918fa
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-21 09:51:54 +0100

    update deps and use node 8

--- test/handlebarsTest.js
+++ test/handlebarsTest.js
@@ -1,69 +1,68 @@
-var should = require('should')
-var handlebars = require('../lib/handlebarsEngine.js')
+const should = require('should')
+const handlebars = require('../lib/handlebarsEngine.js')
 
-describe('handlebars', function () {
+describe('handlebars', () => {
   it('should render html', function () {
-    var html = handlebars('Hey')(null, null)
+    handlebars('Hey')(null, null).should.be.eql('Hey')
+  })
+
+  it('should be able to use helpers', () => {
+    const html = handlebars('{{{a}}}')({ a: function () { return 'Hey' } }, null)
     html.should.be.eql('Hey')
   })
 
-  it('should be able to use helpers', function () {
-    var html = handlebars('{{{a}}}')({ a: function () { return 'Hey' } }, null)
+  it('should be able to use data', () => {
+    const html = handlebars('{{{a}}}')(null, { a: 'Hey' })
     html.should.be.eql('Hey')
   })
 
-  it('should be able to use data', function () {
-    var html = handlebars('{{{a}}}')(null, { a: 'Hey' })
-    html.should.be.eql('Hey')
-  })
-
-  it('should throw when syntax error', function () {
-    should(function () {
+  it('should throw when syntax error', () => {
+    should(() => {
       handlebars('{{#if}}')(null, {})
     }).throw()
   })
 
-  it('should work with jsreport syntax', function () {
-    var html = handlebars('{#image {{b}}}')(null, { b: 'foo' })
+  it('should work with jsreport syntax', () => {
+    const html = handlebars('{#image {{b}}}')(null, { b: 'foo' })
     html.should.be.eql('{#image foo}')
   })
 
-  it('should work with jsreport syntax in many places', function () {
-    var html = handlebars("<img src='{#image {{name}}}'/><img src='{#image {{name2}}}'/>")(null, { name: 'foo', name2: 'bar' })
+  it('should work with jsreport syntax in many places', () => {
+    const html = handlebars("<img src='{#image {{name}}}'/><img src='{#image {{name2}}}'/>")(null, { name: 'foo', name2: 'bar' })
     html.should.be.eql("<img src='{#image foo}'/><img src='{#image bar}'/>")
   })
 })
 
-describe('handlebars full', function () {
-  var jsreport = require('jsreport-core')()
-
-  beforeEach(function () {
-    return jsreport.use(require('../')()).init()
+describe('handlebars full', () => {
+  let jsreport
+  beforeEach(() => {
+    jsreport = require('jsreport-core')()
+    jsreport.use(require('../')())
+    return jsreport.init()
   })
 
-  it('should expose handlebars global object', function () {
-    return jsreport.render({
+  it('should expose handlebars global object', async () => {
+    const res = await await jsreport.render({
       template: {
         content: '{{foo}}',
         engine: 'handlebars',
         recipe: 'html',
         helpers: "function foo() { return handlebars.escapeExpression('a') }"
       }
-    }).then(function (res) {
-      res.content.toString().should.be.eql('a')
     })
+
+    res.content.toString().should.be.eql('a')
   })
 
-  it('should expose Handlebars global object', function () {
-    return jsreport.render({
+  it('should expose Handlebars global object', async () => {
+    const res = await jsreport.render({
       template: {
         content: '{{foo}}',
         engine: 'handlebars',
         recipe: 'html',
         helpers: "function foo() { return Handlebars.escapeExpression('a') }"
       }
-    }).then(function (res) {
-      res.content.toString().should.be.eql('a')
     })
+    res.content.toString().should.be.eql('a')
   })
 })
