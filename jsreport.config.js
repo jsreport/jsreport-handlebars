eeb559c87194995ddb73e3c2b85bdae639d5883c
BJR Matos <bjrmatos@gmail.com>
2019-08-21 11:11:38 -0500

    move handlebars to peer dep
    references jsreport/jsreport#606

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  'name': 'handlebars',
-  'main': 'lib/handlebars.js',
-  'hasPublicPart': false
+  name: 'handlebars',
+  main: 'lib/handlebars.js',
+  hasPublicPart: false,
+  optionsSchema: {
+    extensions: {
+      handlebars: {
+        type: 'object',
+        properties: {
+          handlebarsModulePath: {
+            type: 'string',
+            description: 'Specifies a path to a custom handlebars. this module will be used for the template engine evaluation and will be an available module in helpers'
+          }
+        }
+      }
+    }
+  }
 }
--- test/handlebarsTest.js
+++ test/handlebarsTest.js
@@ -1,35 +1,40 @@
 const should = require('should')
 const handlebars = require('../lib/handlebarsEngine.js')
 
+const engineOptions = {
+  handlebarsModulePath: require.resolve('handlebars')
+}
+
 describe('handlebars', () => {
   it('should render html', function () {
-    handlebars('Hey')(null, null).should.be.eql('Hey')
+    handlebars('Hey', engineOptions)(null, null).should.be.eql('Hey')
   })
 
   it('should be able to use helpers', () => {
-    const html = handlebars('{{{a}}}')({ a: function () { return 'Hey' } }, null)
+    const html = handlebars('{{{a}}}', engineOptions)({ a: function () { return 'Hey' } }, null)
     html.should.be.eql('Hey')
   })
 
   it('should be able to use data', () => {
-    const html = handlebars('{{{a}}}')(null, { a: 'Hey' })
+    const html = handlebars('{{{a}}}', engineOptions)(null, { a: 'Hey' })
     html.should.be.eql('Hey')
   })
 
   it('should throw when syntax error', () => {
     should(() => {
-      handlebars('{{#if}}')(null, {})
+      handlebars('{{#if}}', engineOptions)(null, {})
     }).throw()
   })
 
   it('should work with jsreport syntax', () => {
-    const html = handlebars('{#image {{b}}}')(null, { b: 'foo' })
+    const html = handlebars('{#image {{b}}}', engineOptions)(null, { b: 'foo' })
     html.should.be.eql('{#image foo}')
   })
 
   it('should work with jsreport syntax in many places', () => {
     const html = handlebars(
-      "{{name2}} {#child @data.foo={{aHelper}}}<img src='{#image {{name2}}}'/>"
+      "{{name2}} {#child @data.foo={{aHelper}}}<img src='{#image {{name2}}}'/>",
+      engineOptions
     )({ aHelper: () => { return 'a' } }, { name2: 'bar' })
 
     html.should.be.eql("bar {#child @data.foo=a}<img src='{#image bar}'/>")
